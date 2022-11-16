cbbc5a6e5ae46fae55990ab0812bcab5e61f3d74
logankishore <97600598+logankishore@users.noreply.github.com>
2022-11-16 19:48:37 +0530

    Update xman.py

--- xman.py
+++ xman.py
@@ -1,4 +1,3 @@
 if __name__ == '__main__':
     print(Crypto().encrypt("0i6QnSETFywG4Opne/WlKbud/krcUhtxZKPo5iFzh6o="))
-    print(Crypto().decrypt("ygfJhsgEhebdhWrdcf3f=="))
-    print(CryptoPMP(meta={"decrypt_key": "ygfJhsgEhebdhWrdcf3f=="}).decrypt("hjIauAbgtiHGFsEvna=="))
+    
